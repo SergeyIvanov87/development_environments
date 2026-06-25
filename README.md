e1c652ad578dbd89f6d7ac6dff4e9e40ba22b4f4
SergeyIvanov87 <sergius140608@rambler.ru>
2026-06-25 16:04:35 +0200

    Add ssh key generation. TODO need to add to ssh agent

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # to build devel-only
 
-docker buildx build -f system_development/Dockerfile -t sys_devel_env:latest --annotation "runcmd=docker run -it -e \"DISPLAY=$DISPLAY\" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri --name sys_devel_env_cont sys_devel_env:latest" .
+docker buildx build -f system_development/Dockerfile -t is140608/sys_devel_env:latest --annotation "runcmd=docker run -it -e \"DISPLAY=$DISPLAY\" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri --name sys_devel_env_cont is140608/sys_devel_env:latest" .
 
 # to build ai_devel
 
--- system_development/run.sh
+++ system_development/run.sh
@@ -3,6 +3,6 @@
 xhost +local:
 
 #docker run -it -e "DISPLAY=$DISPLAY" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri --mount type=bind,src=./system_development/sources/patches,dst=/patches --name simple_dev_cont sys_devel_env:latest
-docker run -it -e "DISPLAY=$DISPLAY" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri --name sys_devel_env_cont sys_devel_env:latest
+docker run -it -e "DISPLAY=$DISPLAY" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri --name sys_devel_env_cont is140608/sys_devel_env:0.0.1
 
 #development_environment:latest
