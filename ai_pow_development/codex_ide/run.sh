8c613e41c0a78f5c7bf9fa39c67225b5553610fa
SergeyIvanov87 <sergius140608@rambler.ru>
2026-07-09 16:56:12 +0200

    Minor fix for codex_ide run

--- ai_pow_development/codex_ide/run.sh
+++ ai_pow_development/codex_ide/run.sh
@@ -6,9 +6,9 @@ xhost +local:
 docker run -it -e \
     "DISPLAY=$DISPLAY" --mount type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix --device=/dev/dri:/dev/dri \
     -v /run/user/1000/pulse/native:/tmp/pulse-socket -e PULSE_SERVER=unix:/tmp/pulse-socket --device=/dev/snd  \
-    --mount type=bind,src=${WORKSPACE_PATH},dst=/home/user/data \
+    --mount type=bind,src=${WORKSPACE_PATH},dst=/data \
     -v codec_ide_env.sshkeys.volume:/sshkeys \
-    -v codec_ide_env.browser.data.volume:/home/user/.config/mozilla/firefox \
+    -v codec_ide_env.browser.data.volume:/root/.config/mozilla/firefox \
     --name codex_ide_env_cont \
     is140608/codex_ide_env:0.0.1
 
